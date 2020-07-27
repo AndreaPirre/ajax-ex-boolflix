657a535cea33409481f80191f22e63c3e855cbdb
Andrea <andreapirrello1990@gmail.com>
2020-07-27 16:22:15 +0200

    ricerca film e stampa come console log

--- js/script.js
+++ js/script.js
@@ -9,13 +9,63 @@
 // 3.Lingua
 // 4.Voto
 
+$(document).ready(function(){
+
+            var urlBase="https://api.themoviedb.org/3";
+
+            $('#search_input').keydown(function(event){
+            if (event.which==13) {
+              var filmCercato=$('#search_input').val();
+              cercaFilm(filmCercato,urlBase);
+
+                }
+            });
+
+            function stampaFilm(elencofilm){
+
+            for (var i = 0; i < elencofilm.length; i++) {
+              var titoloFilm= elencofilm[i].title;
+              var titoloOriginale= elencofilm[i].original_title;
+              var linguaOriginale= elencofilm[i].original_language;
+              var votoFilm= elencofilm[i].vote_average;
+
+          }};
+
+
+            function cercaFilm(ricercaUtente,urlBase){
+
+                $.ajax({
+                  url:urlBase+"/search/movie",
+                  method:"GET",
+                  data: {
+                    'api_key':"603faf8c2a684dc57112e107f86af1ba",
+                    'query': ricercaUtente,
+                    'language': "it"
+                  },
+                  success:function(data){
+                    var filmtrovati=data.results;
+                    console.log(filmtrovati);
+                    stampaFilm(filmtrovati);
+                  },
+                  error: function(){
+                    alert("Ops, qualcosa è andato storto");
+                }}
+
+            )};
 
 
 
-function init() {
+});
 
-console.log("hello world");
 
-}
 
-$(document).ready(init);
+
+// function init() {
+//
+//
+//
+// console.log("hello world");
+//
+// }
+//
+// $(document).ready(init);
