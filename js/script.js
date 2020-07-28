f90ae7ec4a4883e2e11b4717068788f34d4f6c98
Andrea <andreapirrello1990@gmail.com>
2020-07-28 17:57:51 +0200

    aggiunte serie tv alla ricerca

--- js/script.js
+++ js/script.js
@@ -9,9 +9,21 @@
 // 3.Lingua
 // 4.Voto
 
+// Milestone 2:Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+// così dapermetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,lasciando
+// le restanti vuote (troviamo le icone in FontAwesome).Arrotondiamo sempre per eccesso all’unità successiva,
+// non gestiamo icone mezzepiene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente,
+// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono
+// alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi
+// nel JSON di risposta diversi, simili ma non sempre identici)Qui un esempio di chiamata per le serie tv:
+
 function addSearchClickListener() {
     var target = $('#search_btn');
-    target.click(mostraFilm)
+    target.click(mostraFilm, mostraSerieTv)
+    
+
 }
 
 function mostraFilm(){
@@ -35,7 +47,7 @@ function mostraFilm(){
             target.text('');
             var template = $('#film-template').html();
             var compiled = Handlebars.compile(template);
-            
+
             // ciclo for
             for (var i = 0; i < films.length; i++) {
 
@@ -48,6 +60,42 @@ function mostraFilm(){
         error: function(err) {
             console.log(err);
         }
+    });
+
+}
+
+    function mostraSerieTv(){
+
+        var target = $('#query');
+        var query = target.val();
+        target.val('');
+
+    $.ajax({
+      url:'https://api.themoviedb.org/3/search/tv',
+      method:"GET",
+      data: {
+        'api_key': "603faf8c2a684dc57112e107f86af1ba",
+        'query': query,
+      },
+      success: function(data){
+        var serieTvTrovate=data['results'];
+        var target = $('#results ul');
+        target.text('');
+        var template = $('#serie-template').html();
+        var compiled = Handlebars.compile(template);
+
+        for (var i = 0; i < serieTvTrovate.length; i++) {
+
+            var serie = serieTvTrovate[i];
+            var serieHTML = compiled(serie);
+            target.append(serieHTML);
+
+        }
+
+      },
+      error: function(){
+        alert("Ops, qualcosa è andato storto");
+      }
     });
 }
 
