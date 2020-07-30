02a75b19de4d3db0e3eae4a77a15833e65f74089
Andrea <andreapirrello1990@gmail.com>
2020-07-30 15:17:18 +0200

    aggiunte immagine film e serie tv

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o
-// parzialmente il nome di un film.
-// Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che
-// ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// 1.Titolo
-// 2.Titolo Originale
-// 3.Lingua
-// 4.Voto
 
-// Milestone 2:Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
-// così dapermetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,lasciando
-// le restanti vuote (troviamo le icone in FontAwesome).Arrotondiamo sempre per eccesso all’unità successiva,
-// non gestiamo icone mezzepiene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente,
-// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono
-// alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi
-// nel JSON di risposta diversi, simili ma non sempre identici)Qui un esempio di chiamata per le serie tv:
 
 function addSearchClickListener() {
     var target = $('#search_btn');
@@ -38,7 +19,7 @@ function avviaRicerca() {
 }
 
 
-
+// film
 function mostraFilm(query){
 
 
@@ -69,6 +50,7 @@ function mostraFilm(query){
                 var lang = film['original_language'];
                 film.flag = getFlag(lang);
 
+
                 var filmHTML = compiled(film);
                 target.append(filmHTML);
 
@@ -81,6 +63,7 @@ function mostraFilm(query){
 
 }
 
+// stelle
 function getStars(vote) {
     vote = Math.ceil(vote / 2);
     var voteHTML = '';
@@ -97,6 +80,7 @@ function getStars(vote) {
     return voteHTML;
 }
 
+// bandiere
 function getFlag(lang) {
 
     if (lang === 'it' || lang === 'en') {
@@ -107,7 +91,7 @@ function getFlag(lang) {
         return lang;
 
 }
-
+// serie tv
     function mostraSerieTv(query){
 
         $.ajax({
@@ -149,11 +133,16 @@ function getFlag(lang) {
 }
 
 
+
+
 function init() {
 
     addSearchClickListener();
 
     avviaRicerca();
+
+// debug
+    mostraFilm();
 
 }
 
